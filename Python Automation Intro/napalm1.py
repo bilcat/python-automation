74251d390f8bf14b7cbb38caf4f2a9318f66287d
Bilal Catakoglu <bcatakoglu@gmail.com>
2022-01-22 21:00:32 +1100

    Napalm: Get facts from a switch and print interfaces list and counters

--- Python Automation Intro/napalm1.py
+++ Python Automation Intro/napalm1.py
@@ -7,3 +7,9 @@ iosvl2.open()
 
 ios_output = iosvl2.get_facts()
 print(json.dumps(iosvl2, indent=4))
+
+ios_output_int = iosvl2.get_interfaces()
+print(json.dumps(ios_output_int, indent=4))
+
+ios_output_count = iosvl2.get_interfaces_counters()
+print(json.dumps(ios_output_count, indent=4))
